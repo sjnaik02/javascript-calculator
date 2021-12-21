2f50b37f6324f62a5ed229e186d973918e65317d
Shourya Naik <sjnaik02@gmail.com>
2021-12-21 20:56:44 +0530

    🚀 added basic input functionality

--- src/index.js
+++ src/index.js
@@ -2,29 +2,43 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css'
 
-class App extends React.Component{
-    constructor(props){
+const buttons = [
+    { name: 'one', value: 1 },
+    { name: 'two', value: 2 },
+    { name: 'three', value: 3 },
+    { name: 'four', value: 4 },
+    { name: 'five', value: 5 },
+    { name: 'six', value: 6 },
+    { name: 'seven', value: 7 },
+    { name: 'eight', value: 8 },
+    { name: 'nine', value: 9 },
+    { name: 'ten', value: 0 }
+]
+
+class App extends React.Component {
+    constructor(props) {
         super(props);
         this.state = {
-            buttons: [
-                {name: 'one', value: 1},
-                {name: 'two', value: 2},
-                {name: 'three', value: 3},
-                {name: 'four', value: 4},
-                {name: 'five', value: 5},
-                {name: 'six', value: 6},
-                {name: 'seven', value: 7},
-                {name: 'eight', value: 8},
-                {name: 'nine', value: 9},
-                {name: 'ten', value: 0},
-            ],
+            equation: '0',
         }
+        this.handleClick = this.handleClick.bind(this);
     }
 
-    render(){
-        return(
-            <div> Hello World </div>
+    handleClick(num){
+        this.setState({ equation : this.state.equation+ num });
+    }
+
+    render() {
+        return (
+            <div className='wrapper'>
+                <h1>{this.state.equation}</h1>
+                {buttons.map((item) =>
+                    <button id={item.name} onClick={()=> this.handleClick(item.value)}> {item.value} </button>)
+                }
+            </div>
+
         );
     }
 }
+
 ReactDOM.render(<App />, document.getElementById('root'));
