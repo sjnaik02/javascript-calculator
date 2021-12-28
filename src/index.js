0039853bd73fb7b7e968125e0b48e56bd3303fa3
Shourya Naik <sjnaik02@gmail.com>
2021-12-29 00:29:08 +0530

    🧮  added calculation capability

--- src/index.js
+++ src/index.js
@@ -12,59 +12,87 @@ const nums = [
     { name: 'seven', value: 7 },
     { name: 'eight', value: 8 },
     { name: 'nine', value: 9 },
-    { name: 'ten', value: 0 }
+    { name: 'ten', value: 0 },
+    { name: 'decimal', value: '.' }
 ]
 
-const operators =[
-    {name : 'add', value: '+'},
-    {name : 'subtract', value: '-'},
-    {name : 'multiply', value: '*'},
-    {name : 'divide', value: '/'}
+const operators = [
+    { name: 'add', value: '+' },
+    { name: 'subtract', value: '-' },
+    { name: 'multiply', value: '*' },
+    { name: 'divide', value: '/' }
 ]
-
-const other = [
-    {name : 'equals', value: '='},
-    {name : 'clear', value: 'AC'}
-]
-
-
 
 class App extends React.Component {
     constructor(props) {
         super(props);
         this.state = {
             fragment: '',
-            equation: ''
+            activeEquation: '',
+            lastEquation: '',
+            lastOperator: '',
+            output: '',
         }
         this.handleNumClick = this.handleNumClick.bind(this);
+        this.calculate = this.calculate.bind(this);
+        this.clear = this.clear.bind(this);
     }
 
-    handleNumClick(num){
-        this.setState((state) => ({ 
-            fragment : state.fragment+ num ,
-            equation : state.equation,
+    handleNumClick(num) {
+        this.setState((state) => ({
+            fragment: state.fragment + num,
+            lastOperator: '',
+            activeEquation: state.activeEquation + num,
+            lastEquation: '',
+            output: '',
         }));
     }
 
-    handleOperatorClick(op){
+    handleOperatorClick(op) {
         this.setState((state) => ({
-            fragment : '',
-            equation : state.equation + state.fragment + op 
+            fragment: '',
+            lastOperator: op,
+            activeEquation: state.activeEquation + op,
+            lastEquation: '',
+            output: '',
+        }));
+    }
+
+    calculate() {
+        this.setState((state) => ({
+            lastOperator: '',
+            fragment: '',
+            activeEquation: '',
+            lastEquation: state.activeEquation,
+            output: eval(state.activeEquation),
+        }));
+    }
+
+    clear() {
+        this.setState((state) => ({
+            lastOperator: '',
+            fragment: '',
+            activeEquation: '',
+            lastEquation: '',
+            output: '',
         }));
     }
 
     render() {
         return (
             <div className='wrapper'>
-                <h2 id='equation-display'>{this.state.equation}</h2>
-                <h1 id='display'>{this.state.fragment}</h1>
+                <h2 id='activeEquation-display'>your equation: {this.state.activeEquation ? this.state.activeEquation : this.state.lastEquation}</h2>
+                <h1 id='display'>input: {this.state.fragment ? this.state.fragment : this.state.lastOperator ? this.state.lastOperator : this.state.output}</h1>
                 {nums.map((item) =>
-                    <button id={item.name} onClick={()=> this.handleNumClick(item.value)}> {item.value} </button>)
+                    <button id={item.name} onClick={() => this.handleNumClick(item.value)}> {item.value} </button>)
                 }
 
                 {operators.map((item) =>
-                    <button id={item.name} onClick={()=> this.handleOperatorClick(item.value)}> {item.value} </button>)
+                    <button id={item.name} onClick={() => this.handleOperatorClick(item.value)}> {item.value} </button>)
                 }
+
+                <button id='equals' onClick={this.calculate}> = </button>
+                <button id='clear' onClick={this.clear}> AC </button>
             </div>
         );
     }
